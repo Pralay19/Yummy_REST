68909d98f83904d9e475fcf4a11c0813c539aae5
Pralay <pralaydutta666.pd@gmail.com>
2024-11-19 19:39:35 +0530

    Decided to go with Argon2 inplace of Bcrpt

--- Yummy_REST/src/main/java/com/yummyrest/yummy_rest/configuration/SecurityConfig.java
+++ Yummy_REST/src/main/java/com/yummyrest/yummy_rest/configuration/SecurityConfig.java
@@ -3,13 +3,16 @@ package com.yummyrest.yummy_rest.configuration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
     @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
+    public PasswordEncoder PasswordEncoder() {
+        //return new BCryptPasswordEncoder();
+        return new Argon2PasswordEncoder(16,32,1,16*1024,4);
     }
 }
