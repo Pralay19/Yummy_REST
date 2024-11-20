69a19a3d4a5394ae7a422e578988f18e8e2110cd
Pralay <pralaydutta666.pd@gmail.com>
2024-11-20 11:01:34 +0530

    Annotations added @Repository

--- Yummy_REST/src/main/java/com/yummyrest/yummy_rest/repo/Customer_repo.java
+++ Yummy_REST/src/main/java/com/yummyrest/yummy_rest/repo/Customer_repo.java
@@ -2,10 +2,12 @@ package com.yummyrest.yummy_rest.repo;
 
 import com.yummyrest.yummy_rest.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface Customer_repo extends JpaRepository<Customer, Long> {
     Optional<Customer> findByEmail(String email);
     List<Customer> findByLastName(String lastName);
